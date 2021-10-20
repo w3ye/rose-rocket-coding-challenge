313901119e8aa6836e55172f9408e18029bb2e6b
William Ye <33244107+zeeplo@users.noreply.github.com>
2021-10-20 16:34:03 -0400

    change unassigned order to first

--- client/src/components/DriverColumnItem.jsx
+++ client/src/components/DriverColumnItem.jsx
@@ -1,17 +1,23 @@
+import { Fragment } from "react";
 import OrderItemList from "./OrderItemList";
 
 export default function DriverColumnItem(props) {
   const { driverOrder } = props;
   return (
-    <div className="list-wrapper">
+    <>
       {driverOrder.id === null ? (
-        <div className="list-header left">Unassigned Orders</div>
+        <div className="list-wrapper driver-col-first">
+          <div className="list-header">Unassigned Orders</div>
+          <OrderItemList orders={driverOrder.orders} />
+        </div>
       ) : (
-        <div className="list-header">
-          Drivers {driverOrder.first_name} {driverOrder.last_name}{" "}
+        <div className="list-wrapper">
+          <div className="list-header">
+            Drivers {driverOrder.first_name} {driverOrder.last_name}{" "}
+          </div>
+          <OrderItemList orders={driverOrder.orders} />
         </div>
       )}
-      <OrderItemList orders={driverOrder.orders} />
-    </div>
+    </>
   );
 }
